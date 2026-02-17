7c59f8ed199ca4845e14f45a838dd6c136487604
K1rL3s <kirill.lesovoi@mail.ru>
2026-02-17 14:03:28 +0300

    Алиас `maxo.filters` к `maxo.routing.filters`

---
+++ src/maxo/filters.py
@@ -0,0 +1,44 @@
+import warnings
+
+warnings.warn(
+    "Алиас `maxo.filters` сделан для удобного портирования ботов с `aiogram` "
+    "и будет удален в будущих версиях. "
+    "Пожалуйста, обновите импорты на 'from maxo.routing.filters import ...'",
+    FutureWarning,
+    stacklevel=2,
+)
+
+# `MagicFilter` and `MagicData` in maxo.integrations.magic_filter
+
+from maxo.routing.filters.always import AlwaysFalseFilter, AlwaysTrueFilter
+from maxo.routing.filters.base import BaseFilter
+from maxo.routing.filters.command import Command, CommandStart
+from maxo.routing.filters.deeplink import DeeplinkFilter
+from maxo.routing.filters.exception import ExceptionMessageFilter, ExceptionTypeFilter
+from maxo.routing.filters.logic import (
+    AndFilter,
+    InvertFilter,
+    OrFilter,
+    and_f,
+    invert_f,
+    or_f,
+)
+from maxo.routing.filters.payload import Payload
+
+__all__ = (
+    "AlwaysFalseFilter",
+    "AlwaysTrueFilter",
+    "AndFilter",
+    "BaseFilter",
+    "Command",
+    "CommandStart",
+    "DeeplinkFilter",
+    "ExceptionMessageFilter",
+    "ExceptionTypeFilter",
+    "InvertFilter",
+    "OrFilter",
+    "Payload",
+    "and_f",
+    "invert_f",
+    "or_f",
+)
